738ed9d4c88dd49147664d7c5f17c2de18bcee80
liuyang <liuyang@dalongyun.com>
2022-02-15 13:58:11 +0800

    test: 添加客户端调试代码

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useRouter} from 'vue-router'
+import {useRouter,} from 'vue-router'
 import {onMounted,computed} from 'vue'
 import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
 const router = useRouter();
@@ -7,6 +7,12 @@ const inset = computed(() => router.currentRoute.value.query.inset);
 const bgColor = computed(() => router.currentRoute.value.query.bgColor);
 const textColor = computed(() => router.currentRoute.value.query.color);
 const showPageNav = computed(() => router.currentRoute.value.query.showPageNav);
+onMounted(() => {
+  // 测试是否接收到参数
+  if(router.currentRoute.value.query.debugger){
+    alert(JSON.stringify(router.currentRoute.value.query))
+  }
+})
 </script>
 
 <template>
