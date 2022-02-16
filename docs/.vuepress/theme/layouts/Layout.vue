a5f4da25bd2a68e289bfcb1d86c884c72e8082e2
liuyang <liuyang@dalongyun.com>
2022-02-16 14:57:16 +0800

    feat: 改变获取地址栏参数的方法

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,20 +1,40 @@
 <script setup lang="ts">
 import {useRouter,} from 'vue-router'
-import {onMounted,computed} from 'vue'
+import {onBeforeMount,computed,ref} from 'vue'
 import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
 const router = useRouter();
-const inset = computed(() => router.currentRoute.value.query.inset);
-const bgColor = computed(() => router.currentRoute.value.query.bgColor);
-const textColor = computed(() => router.currentRoute.value.query.color);
-const showPageNav = computed(() => router.currentRoute.value.query.showPageNav);
-onMounted(() => {
-  // 测试是否接收到参数
-  console.log(router.currentRoute.value.query)
+// const state = reactive()
+const queryObj = ref();
+const inset =ref('false')
+const bgColor =ref('')
+const textColor =ref('')
+const showPageNav =ref('')
+const getQueryObject = (url?: any) =>  {
+  url = url == null ? window.location.href : url;
+  const search = url.substring(url.lastIndexOf('?') + 1);
+  const obj: any = {};
+  const reg = /([^?&=]+)=([^?&=]*)/g;
+  search.replace(reg, (rs: string, $1: string, $2: string) => {
+    const name = decodeURIComponent($1);
+    let val = decodeURIComponent($2);
+    val = String(val);
+    obj[name] = val;
+    return rs;
+  });
+
+  return obj;
+}
+onBeforeMount(() => {
+  queryObj.value = getQueryObject()
+  inset.value = queryObj.value.inset;
+  bgColor.value =queryObj.value.bgColor;
+  textColor.value =queryObj.value.color;
+  showPageNav.value =queryObj.value.showPageNav;
 })
 </script>
 
 <template>
-<div class="inset">
+<div :class="[inset ? 'inset' : '']">
   <ParentLayout></ParentLayout>
 </div>
   
