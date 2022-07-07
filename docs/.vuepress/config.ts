f39089359b2b3e3bf27264ca770169ebb648dc47
liuyang <liuyang@dalongyun.com>
2022-07-07 17:37:20 +0800

    feat: 添加head base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,36 +4,36 @@ import path from 'path';
 import fs from 'fs';
 
 // 自动获取侧边栏
-  function readFileList(dir,filesList = [],isChild = false) {
-    const pathDir = `./docs/${dir}`
+function readFileList(dir, filesList = [], isChild = false) {
+  const pathDir = `./docs/${dir}`
   let files = fs.readdirSync(pathDir);
-  files.forEach((item,index) => {
-    const fullPath = path.join(pathDir,item);
+  files.forEach((item, index) => {
+    const fullPath = path.join(pathDir, item);
     const stat = fs.statSync(fullPath);
     // 是否还有子目录
-   const noList = ['docs/.vuepress','docs/.DS_Store','docs/zh']
-    if(stat.isDirectory() && !noList.includes(fullPath)){
-      const link = fullPath.replace(`docs`,'')
+    const noList = ['docs/.vuepress', 'docs/.DS_Store', 'docs/zh']
+    if (stat.isDirectory() && !noList.includes(fullPath)) {
+      const link = fullPath.replace(`docs`, '')
       filesList.push({
         text: path.basename(fullPath),
         link,
-        children:[]
+        children: []
       })
-      readFileList(path.join(dir,item), filesList,true)
+      readFileList(path.join(dir, item), filesList, true)
     } else {
       const name = path.extname(fullPath)
       const noHome = path.basename(fullPath) != 'README.md'
-      if(name == '.md' && noHome){
-        const link = fullPath.replace(`docs`,'')
+      if (name == '.md' && noHome) {
+        const link = fullPath.replace(`docs`, '')
         const sidebar = {
           text: path.basename(fullPath, path.extname(fullPath)),
           link,
         }
-        if(isChild){
-        
+        if (isChild) {
+
           const len = filesList.length - 1;
-          if(filesList[len].children){
-            if(index == 0){
+          if (filesList[len].children) {
+            if (index == 0) {
               filesList[len].link = link
             }
             filesList[len].children.push(link)
@@ -42,7 +42,7 @@ import fs from 'fs';
           filesList.push(link)
         }
       }
-     
+
     }
   })
 
@@ -50,11 +50,11 @@ import fs from 'fs';
 }
 
 export default defineUserConfig<DefaultThemeOptions>({
-    // 站点配置
+  // 站点配置
   lang: 'en-US',
   title: 'Joyark docs',
   description: 'docs work',
-  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }], ['base', { target: '_parent' }]],
   // 多语言
   locales: {
     // 键名是该语言所属的子路径
@@ -71,20 +71,20 @@ export default defineUserConfig<DefaultThemeOptions>({
   theme: path.resolve(__dirname, './theme/index.ts'),
   // 主题配置
   themeConfig: {
-    displayAllHeaders:true,
+    displayAllHeaders: true,
     logo: '/images/logo.png',
     locales: {
       '/': {
         selectLanguageName: 'English',
-        contributors:false,
-        sidebar:readFileList('')
+        contributors: false,
+        sidebar: readFileList('')
       },
       '/zh/': {
         selectLanguageName: '简体中文',
-        selectLanguageText:'选择语言',
-        lastUpdatedText:'上次更新',
-        contributors:false,
-        sidebar:readFileList('zh/')
+        selectLanguageText: '选择语言',
+        lastUpdatedText: '上次更新',
+        contributors: false,
+        sidebar: readFileList('zh/')
       },
     },
   },
